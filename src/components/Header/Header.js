3fa030ff1943f84b81ec270c30bc65f777934c10
Vlad Konovalchuk <berlitio600@gmail.com>
2019-01-27 01:16:07 +0200

    Add initial dependencies; Create folder for Footer and Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export class Header extends React.PureComponent {
+    render() {
+        return (
+            <header>
+                Header
+            </header>
+        )
+    }
+}
